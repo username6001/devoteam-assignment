7038a5a20d81346431ef81f4d9e6bb48d0232226
Evgeny <username6001@yandex.ru>
2024-05-27 04:14:37 +0400

    add BarcodeService; fix jackson dependency

--- src/main/java/com/devoteam/barcode/BarcodeResource.java
+++ src/main/java/com/devoteam/barcode/BarcodeResource.java
@@ -7,6 +7,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.jboss.resteasy.reactive.multipart.FileUpload;
 
 import java.io.IOException;
@@ -23,13 +24,13 @@ import java.util.List;
 public class BarcodeResource {
 
     @Inject
-    private EntityManager em;
+    private BarcodeService barcodeService;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/list")
     public List<Barcode> get() {
-        return em.createQuery("from Barcode", Barcode.class).getResultList();
+        return barcodeService.getBarcodes();
     }
 
     @POST
@@ -37,40 +38,9 @@ public class BarcodeResource {
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/upload-csv")
     @Transactional
-    public String uploadCsv(@FormParam("file") FileUpload file) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        try {
-            List<List<String>> list = new ArrayList<>();
-            List<Barcode> barcodes = new ArrayList<>();
-            try (Reader reader = Files.newBufferedReader(file.uploadedFile())) {
-                try (CSVReader csvReader = new CSVReader(reader)) {
-                    String[] line;
-                    while ((line = csvReader.readNext()) != null) {
-                        // skip header
-                        if ("Barcode".equals(line[0])) {
-                            continue;
-                        }
-                        Barcode barcode = new Barcode();
-                        // Barcode,Category,ItemName,SellingPrice,ManufacturingDate,ExpiryDate,Quantity
-                        barcode.barcode = line[0];
-                        barcode.category = line[1];
-                        barcode.itemName = line[2];
-                        barcode.sellingPrice = line[3] == null ? null : new BigDecimal(line[3]);
-                        barcode.manufacturingDate = line[4] == null ? null : LocalDate.parse(line[4], formatter);
-                        barcode.expiryDate = line[5] == null ? null : LocalDate.parse(line[5], formatter);
-                        barcode.quantity = line[6] == null ? 0 : Long.parseLong(line[6]);
-                        em.persist(barcode);
-                        barcodes.add(barcode);
-                        list.add(Arrays.asList(line));
-                    }
-                } catch (CsvValidationException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            return barcodes.toString();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public Response uploadCsv(@FormParam("file") FileUpload file) {
+        barcodeService.storeCsvFile(file);
+        return Response.ok().build();
     }
 
     @POST
---
+++ src/main/java/com/devoteam/barcode/BarcodeService.java
@@ -0,0 +1,59 @@
+package com.devoteam.barcode;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import org.jboss.resteasy.reactive.multipart.FileUpload;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@ApplicationScoped
+public class BarcodeService {
+
+    @Inject
+    private EntityManager em;
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public List<Barcode> getBarcodes() {
+        return em.createQuery("from Barcode", Barcode.class).getResultList();
+    }
+
+    private Barcode csvStringToBarcode(String[] csvLine) {
+        Barcode barcode = new Barcode();
+        // Barcode,Category,ItemName,SellingPrice,ManufacturingDate,ExpiryDate,Quantity
+        barcode.barcode = csvLine[0];
+        barcode.category = csvLine[1];
+        barcode.itemName = csvLine[2];
+        barcode.sellingPrice = csvLine[3] == null ? null : new BigDecimal(csvLine[3]);
+        barcode.manufacturingDate = csvLine[4] == null ? null : LocalDate.parse(csvLine[4], formatter);
+        barcode.expiryDate = csvLine[5] == null ? null : LocalDate.parse(csvLine[5], formatter);
+        barcode.quantity = csvLine[6] == null ? 0 : Long.parseLong(csvLine[6]);
+        return barcode;
+    }
+
+    public void storeCsvFile(FileUpload file) {
+        try (Reader reader = Files.newBufferedReader(file.uploadedFile());
+             CSVReader csvReader = new CSVReader(reader)) {
+            String[] line;
+            while ((line = csvReader.readNext()) != null) {
+                // skip header
+                if ("Barcode".equals(line[0])) {
+                    continue;
+                }
+                Barcode barcode = csvStringToBarcode(line);
+                em.persist(barcode);
+            }
+        } catch (CsvValidationException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
